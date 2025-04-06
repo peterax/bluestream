27464f4e891a596a852c9a7b1d561bbfe5df16d8
peterax <peter@reducks.se>
2025-04-06 16:41:57 +0200

    Create autopair.sh

---
+++ autopair.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+bluetoothctl << EOF
+connect 54:42:49:45:43:E8
+EOF
+sleep 10
+bluetoothctl << EOF
+connect 54:42:49:45:43:E8
+EOF
+sleep 10
+bluetoothctl << EOF
+connect 54:42:49:45:43:E8
+EOF
+sleep 10
+bluetoothctl << EOF
+connect 54:42:49:45:43:E8
+EOF
+sleep 10
+bluetoothctl << EOF
+connect 54:42:49:45:43:E8
+EOF
+sleep 10
+bluetoothctl << EOF
+connect 54:42:49:45:43:E8
+EOF
