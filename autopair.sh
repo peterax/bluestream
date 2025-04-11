45902c9a0824e212c2699be7c9c2364e62112c8d
peterax <peter@reducks.se>
2025-04-11 08:10:03 +0200

    Update autopair.sh

--- autopair.sh
+++ autopair.sh
@@ -1,24 +1,30 @@
-#!/bin/bash
+ GNU nano 5.4                                  scripts/autopair *                                          #!/bin/bash
 bluetoothctl << EOF
-connect 54:42:49:45:43:E8
+connect 20:18:5B:84:65:CA
 EOF
+mpc play
 sleep 10
 bluetoothctl << EOF
-connect 54:42:49:45:43:E8
+connect 20:18:5B:84:65:CA
 EOF
+mpc play
 sleep 10
 bluetoothctl << EOF
-connect 54:42:49:45:43:E8
+connect 20:18:5B:84:65:CA
 EOF
+mpc play
 sleep 10
 bluetoothctl << EOF
-connect 54:42:49:45:43:E8
+connect 20:18:5B:84:65:CA
 EOF
+mpc play
 sleep 10
 bluetoothctl << EOF
-connect 54:42:49:45:43:E8
+connect 20:18:5B:84:65:CA
 EOF
+mpc play
 sleep 10
 bluetoothctl << EOF
-connect 54:42:49:45:43:E8
+connect 20:18:5B:84:65:CA
 EOF
+mpc play
